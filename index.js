826e7249dbe8c6f84aaa26df95b0c332e102c91f
Andre Wiggins <andrewiggins@live.com>
2020-06-22 18:37:41 -0700

    Implement cli

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log(42);
+#!/usr/bin/env node
+
+import fs from "fs";
+import { constants as brotliConstants } from "zlib";
+import sade from "sade";
+import prettyBytes from "pretty-bytes";
+import brotliModule from "brotli-size";
+
+sade("brotli-size [file]", true)
+	.version("0.0.0")
+	.describe("Get the brotli size of a file or stdin")
+	.example("unicorn.png")
+	.example("unicorn.png --raw")
+	.option("--raw", "Display value in bytes", false)
+	.option(
+		"-m --mode",
+		"Mode the brotli algorithm should run in. Must be one of NodeJS's zlib.constants.BROTLI_MODE_* values (e.g. 0 = DEFAULT, 1 = TEXT, 2 = FONT)",
+		brotliConstants.BROTLI_DEFAULT_MODE
+	)
+	.option(
+		"-q --quality",
+		"Quality of compression. Must be between NodeJS's zlib.constants.BROTLI_MIN_QUALITY and zlib.constants.BROTLI_MAX_QUALITY. Default is MAX_QUALITY.",
+		brotliConstants.BROTLI_MAX_QUALITY
+	)
+	.action(run)
+	.parse(process.argv);
+
+/**
+ * @param {string} input
+ * @param {{ mode: number; quality: number; raw: boolean }} options
+ */
+function run(input, options) {
+	if (!input && process.stdin.isTTY) {
+		console.error("Specify a file path");
+		process.exit(1);
+	}
+
+	const source = input ? fs.createReadStream(input) : process.stdin;
+
+	// @ts-ignore
+	const { stream: brotliSize } = brotliModule;
+	source.pipe(brotliSize(options)).on("brotli-size", (size) => {
+		console.log(options.raw ? size : prettyBytes(size));
+	});
+}
--- test.js
+++ test.js
@@ -1,16 +1,66 @@
+import * as fs from "fs";
 import { spawnSync } from "child_process";
 import path from "path";
 import { fileURLToPath } from "url";
 import test from "tape";
+import brotliModule from "brotli-size";
 
 // @ts-ignore
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const repoRoot = (...args) => path.join(__dirname, ...args);
+const testPath = repoRoot("test.js");
 
-test("main", async (t) => {
-	const result = spawnSync(process.execPath, [repoRoot("index.js")], {
+// @ts-ignore
+const { fileSync } = brotliModule;
+
+/**
+ * @param {string[]} args
+ * @param {string} [stdin]
+ */
+function runCli(args, stdin) {
+	args = [repoRoot("index.js"), ...args];
+
+	/** @type {import("child_process").SpawnSyncOptionsWithStringEncoding} */
+	const options = {
+		input: stdin ? stdin : undefined,
 		encoding: "utf8",
-	});
+	};
 
-	t.equal(result.stdout, "42\n", "42 is the output");
+	const result = spawnSync(process.execPath, args, options);
+
+	if (result.stderr) {
+		throw new Error(`Process exited with non-empty stderr: "${result.stderr}"`);
+	}
+
+	if (result.status !== 0) {
+		throw new Error("Process exited with non-zero status: " + result.status);
+	}
+
+	return result.stdout;
+}
+
+test("file", async (t) => {
+	const stdout = runCli(["test.js"]);
+	t.match(stdout.trim(), /^\d+ B$/, "Outputs formatted byte count");
+});
+
+test("file raw", async (t) => {
+	const stdout = runCli(["test.js", "--raw"]);
+	t.equal(parseInt(stdout, 10), fileSync(testPath), "Outputs raw byte count");
+});
+
+test("stdin raw", async (t) => {
+	const stdin = fs.readFileSync(testPath, "utf-8");
+	const stdout = runCli(["--raw"], stdin);
+	t.equal(parseInt(stdout, 10), fileSync(testPath), "Accepts stdin stream");
+});
+
+test("version", async (t) => {
+	const pkg = JSON.parse(fs.readFileSync(repoRoot("package.json"), "utf-8"));
+	const stdout = runCli(["--version"]);
+	t.equal(
+		stdout.trim(),
+		`brotli-size, ${pkg.version}`,
+		"Version matches string in package.json"
+	);
 });
